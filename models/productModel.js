da21e70851224637cd5145e8cd992b1b6e8e888d
ZainaKabbani <zainakabbani07@gmail.com>
2023-05-12 17:56:20 +0200

    isProductOwner, API Features, product address

--- models/productModel.js
+++ models/productModel.js
@@ -1,49 +1,61 @@
 const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate");
+const { lookUpGeoJSON } = require("geojson-places");
+const { lookUpRaw } = require("geojson-places");
 
-const productSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Enter the product name"],
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Enter the product name"],
+    },
+    description: {
+      type: String,
+      required: [true, "Enter the product description"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Enter the product price"],
+    },
+    image: {
+      type: String,
+      required: [true, "Enter the product image"],
+    },
+    quantity: {
+      type: Number,
+      required: [true, "Enter the product quantity"],
+    },
+    address: {
+      type: [Number],
+      required: [true, "Enter the product address"],
+    },
+    rating: {
+      type: Number,
+    },
+    categories: {
+      type: String,
+      enum: ["clothes"],
+      required: [true, "Enter the product categorie"],
+    },
+    status: {
+      type: String,
+      enum: ["new", "old"],
+      required: [true, "Enter the product status new or old"],
+    },
+    userId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+    },
   },
-  description: {
-    type: String,
-    required: [true, "Enter the product description"],
-  },
-  price: {
-    type: Number,
-    required: [true, "Enter the product price"],
-  },
-  image: {
-    type: String,
-    required: [true, "Enter the product image"],
-  },
-  quantity: {
-    type: Number,
-    required: [true, "Enter the product quantity"],
-  },
-  address: {
-    type: [Number],
-    required: [true, "Enter the product address"],
-  },
-  rating: {
-    type: Number,
-  },
-  categories: {
-    type: String,
-    enum: ['clothes'],
-    required: [true, "Enter the product categorie"],
-  },
-  status:{
-    type: String,
-    enum: ['new', 'old'],
-    required: [true, "Enter the product status new or old"],
-  },
-  userId:{
-    type: mongoose.Schema.ObjectId,
-    ref: 'User'
-  }
+  { toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+productSchema.virtual("readableAddress").get(function () {
+  // latitude/longitude
+ // lookUpRaw(43.73828, 7.42542);
+ return lookUpRaw(this.address[0], this.address[1]);
 });
-
-const Product = mongoose.model('Product', productSchema);
+productSchema.plugin(mongoosePaginate);
+const Product = mongoose.model("Product", productSchema);
 
 module.exports = Product;
--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -1,3 +1,5 @@
+const Product = require("./../models/productModel");
+
 class APIFeatures {
   constructor(query, queryString) {
     (this.query = query), (this.queryString = queryString);
@@ -13,5 +15,43 @@ class APIFeatures {
     return this;
   }
 
-  sort() {}
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("_createdAt");
+    }
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(",").join(" ");
+
+      this.query = this.query.select(fields);
+    } else {
+      this.query.select("-__v");
+    }
+    return this;
+  }
+
+  paginate(modelName) {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+
+    this.query = modelName.paginate(
+      {},
+      { page: page, limit: limit },
+      (err, result) => {
+        if (err) {
+          return res.status(500);
+        }
+      }
+    );
+    return this;
+  }
 }
+
+module.exports = APIFeatures;
