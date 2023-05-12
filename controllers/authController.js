9b72017f8c239924e0842b3b2d7dddc8dbf6d685
shaimaa <shaimaaalmously@gmail.com>
2023-05-12 20:07:04 +0300

    updataPassword, upadateProfile, deleteAccount

--- controllers/authController.js
+++ controllers/authController.js
@@ -216,5 +216,24 @@ exports.resetPassword = catchAsync(async(req, res, next)=>{
      status: 'success',
      massage: 'password reseted successfully'
    })
+});
 
-})
+//updatePassword
+ exports.updatePassword = catchAsync(async (req, res, next) => {
+
+  const user = await User.findById(req.user.id).select('+password');
+
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your current password is wrong.', 401));
+  }
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: "success",
+    token
+  });
+});
---
+++ controllers/userController.js
@@ -0,0 +1,61 @@
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+//filter function
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
+//getUsers
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+    const users = await User.find();
+  
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      results: users.length,
+      data: {
+        users
+      }
+    });
+  });
+
+  //update profile
+  exports.updateProfile =catchAsync(async (req, res, next) => {
+    if (req.body.password || req.body.passwordConfirm) {
+      return next(
+        new AppError(
+          'This route is not for password updates. Please use /updateMyPassword.',
+          400
+        )
+      );
+    }
+  const filteredBody = filterObj(req.body, 'fullName', 'email','imag','phone');
+
+ const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+  new: true,
+  runValidators: true
+});
+
+res.status(200).json({
+  status: 'success',
+  data: {
+    user: updatedUser
+  }
+});
+});
+
+//delete account
+exports.deleteAccount = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
